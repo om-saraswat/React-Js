ff5e61d0007bcf581b6c75c34adc5a2ae52e3803
Om Saraswat <sharmaom27022004@gmail.com>
2025-02-28 15:33:25 +0530

    ....

---
+++ 11-todo-reduxtoolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import AddTodo from "./components/Addtodo"
+import Todo from "./components/todo"
+
+function App() {
+  
+
+  return (
+    <>
+     <AddTodo/>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
