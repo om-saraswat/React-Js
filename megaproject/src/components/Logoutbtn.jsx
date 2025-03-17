5d0dbfa104d491e847e2de49e721ff528c58d2a8
Om Saraswat <sharmaom27022004@gmail.com>
2025-03-17 06:28:35 +0530

    .

--- megaproject/src/components/Logoutbtn.jsx
+++ megaproject/src/components/Logoutbtn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import authservice from '../../appwrite/auth';
+import {logout} from '../Store/authslice'
 
 function logoutbtn() {
     const dispatch = useDispatch();
@@ -13,7 +14,7 @@ function logoutbtn() {
     return (
         <button
         className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-        onClick={logoutHandler}
+        onClick={logouthandler}
         >Logout</button>
         
   )
